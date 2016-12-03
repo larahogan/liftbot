d7b51ecd97508970762b49281476a917a93fbd16
lhogan <lara@etsy.com>
2016-12-02 22:20:29 -0500

    getting it to save exercises initially

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 
+workouts = {}
+
+
 def store_workout(lift_type, when, *args):
 	print lift_type, when, args
 
+	exercises = workouts.get(when, None)
+
+	if exercises is None:
+		workouts[when] = {lift_type: args}
+	else:
+		if lift_type not in exercises:
+			exercises[lift_type] = args
+		else:
+			exercises[lift_type] += args
 
 while True:
 	s = raw_input("> ")
